15bd2a2150f9f7b5242c6ad4a536a1a2ef06a39e
Severo-Yannick <severoyannick@gmail.com>
2022-11-05 08:15:57 +0100

    starting project

---
+++ src/Home.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Home from './pages/Home';
+
+test('renders link', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/Freelance Agency/i);
+  expect(linkElement).toBeInTheDocument();
+});
