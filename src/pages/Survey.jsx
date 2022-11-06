5c1fb7bfae51bd65bd64d4706d0c61e99d27be73
Severo-Yannick <severoyannick@gmail.com>
2022-11-06 16:02:12 +0100

    fetch survey data

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,16 +1,26 @@
 import { useParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { URL } from '../utils/constants'
 
 const Survey = () => {
   const { questionNumber } = useParams()
   const [currentNumber, setCurrentNumber] = useState(parseInt(questionNumber))
+  const [surveyData, setSurveyData] = useState({})
+  const surveyLength = Object.keys(surveyData).length
+
+  useEffect(() => {
+    fetch(`${URL}survey`)
+      .then((response) => response.json())
+      .then((data) => setSurveyData(data.surveyData))
+      .catch((error) => console.error('Error fetching survey data', error))
+  }, [])
 
   return (
     <div>
       <h1>Questionnaire 🧮</h1>
       <h2>Question {currentNumber}</h2>
-      <p>Intitulé de la question ?</p>
+      <p>{surveyData[currentNumber]}</p>
       <Link
         to={`/survey/${currentNumber - 1}`}
         style={{ pointerEvents: currentNumber <= 1 ? 'none' : '' }}
@@ -18,7 +28,7 @@ const Survey = () => {
       >
         Précédent
       </Link>
-      {currentNumber < 10 ? (
+      {currentNumber < surveyLength ? (
         <Link
           to={`/survey/${currentNumber + 1}`}
           onClick={() => setCurrentNumber(currentNumber + 1)}
